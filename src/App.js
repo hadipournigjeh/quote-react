e0f1dd7f5a14287d8c91508d491e4ea627475bd4
Hadi Allahverdi Pournigjeh <108987953+hadipournigjeh@users.noreply.github.com>
2023-04-01 16:03:16 +0100

    made button

--- src/App.js
+++ src/App.js
@@ -15,22 +15,43 @@ function App() {
     .catch(error => console.log(error))
   },[])
 
+  function randomQuote() {
+    fetch("https://hadi-quotes.glitch.me/quotes/random")
+    .then(res => res.json())
+    .then(data => setData([data]))
+    .catch(error => console.log(error))
+  }
+
+  function handlerAll () {
+    fetch("https://hadi-quotes.glitch.me/quotes")
+    .then(res => res.json())
+    .then(data => setData(data))
+    .catch(error => console.log(error))
+  }
+
   const [data, setData] = useState([]);
 
   return (
     <div className="App">
-      <ul>
-        {data.map((element, index) => {
-          return <li key={index}>
-                  <p>
-                    {element.quote}
-                  </p>
-                  <h3>
-                    {element.author}
-                  </h3>
-                </li>
-        })}
-      </ul>
+      <div className='header-btn'>
+        <button className='btn-all' onClick={handlerAll}>All</button>
+        <button className='btn-random' onClick={randomQuote}>Random</button>
+      </div>
+      
+      <div className='all-quote'>
+        <ul className='bg-ul'>
+          {data.map((element, index) => {
+            return <li className='li-list' key={index}>
+                    <p>
+                      {element.quote}
+                    </p>
+                    <h3>
+                      {element.author}
+                    </h3>
+                  </li>
+          })}
+        </ul>
+      </div>
     </div>
   );
 }
