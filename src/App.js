1aa7421422e659a5b0a388b2d8e67e26adf6a089
Hadi Allahverdi Pournigjeh <108987953+hadipournigjeh@users.noreply.github.com>
2023-04-15 15:59:30 +0100

    made search inout

--- src/App.js
+++ src/App.js
@@ -8,39 +8,67 @@ import React, {useEffect, useState}  from 'react';
 
 function App() {
 
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+
+
   useEffect(()=>{
     fetch("https://hadi-quotes.glitch.me/quotes")
     .then(res => res.json())
-    .then(data => setData(data))
+    .then(data =>{ setData(data);setFilteredData(data)})
     .catch(error => console.log(error))
   },[])
 
   function randomQuote() {
     fetch("https://hadi-quotes.glitch.me/quotes/random")
     .then(res => res.json())
-    .then(data => setData([data]))
+    .then(val => setFilteredData(val))
     .catch(error => console.log(error))
   }
 
   function handlerAll () {
     fetch("https://hadi-quotes.glitch.me/quotes")
     .then(res => res.json())
-    .then(data => setData(data))
+    .then(data => setFilteredData(data))
     .catch(error => console.log(error))
   }
 
-  const [data, setData] = useState([]);
+  function searchHandler (value) {
+    setFilteredData(data)
+    console.log(value)
+    // fetch(`https://hadi-quotes.glitch.me/quotes/search?word=${value}`)
+    // .then(res => res.json())
+    // .then(data => setData(data))
+    const filteredItems  = data.filter((item)=> 
+    item["quote"].includes(value) || 
+    item["author"].includes(value))
+    console.log(filteredItems,"Filtered  Items");
+    console.log(filteredData,"FilteredDate")
+setFilteredData(filteredItems)
+    
+  }
 
   return (
     <div className="App">
-      <div className='header-btn'>
-        <button className='btn-all' onClick={handlerAll}>All</button>
-        <button className='btn-random' onClick={randomQuote}>Random</button>
-      </div>
+
+      <header>
+        <div className='btn-wrap'>
+          <div className='header-btn-div'>
+            <button className='btn-all header-btn' onClick={handlerAll}>All</button>
+            <button className='btn-random header-btn' onClick={randomQuote}>Random</button>
+          </div>
+          <div className='header-btn-div'>
+            <input className='search-input' type="text" placeholder='Search' onChange={(event) => {
+              searchHandler(event.target.value)
+            }}/>
+          </div>
+        </div>
+      </header>
+      
       
       <div className='all-quote'>
         <ul className='bg-ul'>
-          {data.map((element, index) => {
+          {filteredData.map((element, index) => {
             return <li className='li-list' key={index}>
                     <p>
                       {element.quote}
